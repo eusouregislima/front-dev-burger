0b6ce1fb323e5a82f2a7926005996e1804b5c05b
eusouregislima <danilolima.cogic@gmail.com>
2022-10-11 09:08:22 -0300

    montagem da tela de login

--- src/Container/Login/index.js
+++ src/Container/Login/index.js
@@ -1,10 +1,79 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import LoginImg from '../../assets/loginImg.svg'
+import Logo from '../../assets/logo.png'
+
+import {
+  Container,
+  LoginImage,
+  ContainerItens,
+  Input,
+  Button,
+  SignInLink,
+  Label,
+  ErrorMessage,
+} from './styles'
 
 function Login() {
+  const schema = yup
+    .object({
+      email: yup
+        .string()
+        .email('Digite um email válido')
+        .required('O email é obrigatório'),
+      password: yup
+        .string()
+        .required('A senha é obrigatória')
+        .min(6, 'A senha deve ter no mínimo 6 dígitos'),
+    })
+    .required()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data) => console.log(data)
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LoginImage src={LoginImg} alt="login-image" />
+      <ContainerItens>
+        <img src={Logo} alt="logo-dev-burger" />
+        <h1>Login</h1>
+
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Label>Email</Label>
+
+          <Input
+            type="email"
+            {...register('email')}
+            error={errors.email?.message}
+          />
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
+          <Label>Senha</Label>
+
+          <Input
+            type="password"
+            {...register('password')}
+            error={errors.password?.message}
+          />
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
+
+          <Button type="submit">Entrar</Button>
+        </form>
+
+        <SignInLink>
+          Não possui conta? <a>Cadastre-se</a>
+        </SignInLink>
+      </ContainerItens>
+    </Container>
   )
 }
 
